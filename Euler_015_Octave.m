b6e0496290058b9606b635d5e3da7c8c309a0db9
Ivan Güttler <36445191+IvanGuettler@users.noreply.github.com>
2020-09-24 14:17:55 +0200

    Number of paths works now.

--- Euler_015_Octave.m
+++ Euler_015_Octave.m
@@ -10,24 +10,21 @@ tic
 
 
 % Grid:
-Nx=2;  %N of cells in X direction
-Ny=2;  %N of cells in Y direction
+Nx=20;  %N of cells in X direction
+Ny=20;  %N of cells in Y direction
 
 NcX=Nx+1; %N of points in X direction
 NcY=Ny+1; %N of points in Y direction
 
-Path=zeros(NcX+1,NcY+1);
+Path=zeros(NcX,NcY);
 
-Path(:  ,1)=1;
-Path(1,:  )=1;
+for c = [2:(NcX*NcY)];
 
-for c = [1:(NcX*NcY)+1];
-
-  for i=[1:NcX+1];
-    Path(i,1) = 1;
-    for j=[1:NcY+1];
-      Path(1,j) = 1;
-      if (i+j-2==c-1)
+  for i=[2:NcX];
+    Path(i,1) = 1;   %--->upper border
+    for j=[2:NcY];
+      Path(1,j) = 1; %--->left border
+      if (i+j==c)
           Path(i,j)=Path(i-1,j)+Path(i,j-1);
       end
 
